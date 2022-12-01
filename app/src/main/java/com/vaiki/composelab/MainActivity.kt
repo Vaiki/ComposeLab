059eb1d1c0238b1324f5ab201aad8b3583cf84f0
vaiki <lifeotworld@gmail.com>
2022-12-01 20:14:22 +0300

    start

--- app/src/main/java/com/vaiki/composelab/MainActivity.kt
+++ app/src/main/java/com/vaiki/composelab/MainActivity.kt
@@ -18,26 +18,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeLabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+
+
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeLabTheme {
-        Greeting("Android")
-    }
 }
